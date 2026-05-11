c4912d8ada63635eca78a7b60f95d74888e7964f
marceloferreiracampos07 <junior87721727@gmail.com>
2026-05-10 23:52:38 -0300

    fix: renomear indexroutes para index e corrigir importacao no server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./src/routes');
 const db = require('./models');
 const errorMiddleware = require('./middlewares/error');
 
--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -11,6 +11,7 @@ module.exports = async (req, res, next) => {
   const [, token] = authHeader.split(' ');
 
   try {
+    const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET);
     const { id, cargo } = decoded;
 
     req.userId = id;
